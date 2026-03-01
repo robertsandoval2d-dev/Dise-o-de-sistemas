93bc34c3b6efa59554eb1d80cb3b08d80f4bea4b
MJVlarZ <mjk03178@gmail.com>
2026-02-28 22:28:59 -0500

    Update ISolicitudFactory.java

--- proyecto-gestion-solicitudes-main/src/main/java/com/campus360/solicitudes/Factory/ISolicitudFactory.java
+++ proyecto-gestion-solicitudes-main/src/main/java/com/campus360/solicitudes/Factory/ISolicitudFactory.java
@@ -1,1 +1,9 @@
+package com.campus360.solicitudes.Factory;
 
+import com.campus360.solicitudes.Dominio.Solicitud;
+import com.campus360.solicitudes.DTOs.ServicioInfoResponse;
+
+public interface ISolicitudFactory {
+    // El contrato: toda fábrica debe saber crear una Solicitud
+    Solicitud crear(ServicioInfoResponse info);
+}
